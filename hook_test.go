9e9874ae838a5e1ab586069634a337c1cacc600e
CRORCR <xingzhiheyi_code@163.com>
2019-01-06 16:09:33 +0800

    remove pkg

--- hook_test.go
+++ hook_test.go
@@ -9,8 +9,8 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
-	. "github.com/sirupsen/logrus"
-	. "github.com/sirupsen/logrus/internal/testutils"
+	. "github.com/CRORCR/logrus"
+	. "github.com/CRORCR/logrus/internal/testutils"
 )
 
 type TestHook struct {
--- hooks/syslog/syslog.go
+++ hooks/syslog/syslog.go
@@ -7,7 +7,7 @@ import (
 	"log/syslog"
 	"os"
 
-	"github.com/sirupsen/logrus"
+	"github.com/CRORCR/logrus"
 )
 
 // SyslogHook to send logs via syslog.
